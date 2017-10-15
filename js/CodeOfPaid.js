62649f4319b08f888f45185d6ff5ee93553a9b6e
MetalSyntax <wonderdxk@gmail.com>
2017-10-15 17:52:30 -0400

    1.0.1

--- js/CodeOfPaid.js
+++ js/CodeOfPaid.js
@@ -11,28 +11,27 @@ var Error = "El ID es muy largo";
 document.getElementById("Error").innerHTML = Error; 
 }    
 if (document.getElementById("IDC").value.length>=5 && document.getElementById("IDC").value.length<=9){
-
+               //0,1,2,3
 var Constante = [9,7,3,1];
 
 var A1 = document.getElementById("IDC").value; 
-var B1 = (A1 / 100000000);
-var B2 = (A1 - 100000000*Math.floor(B1));
-var C1 = (B2 / 10000000);
-var C2 = (B2 - 10000000*Math.floor(C1));
-var D1 = (C2 / 1000000);
-var D2 = (C2 - 1000000*Math.floor(D1));
-var E1 = (D2 / 100000);
-var E2 = (D2 - 100000*Math.floor(E1));
-var F1 = (E2 / 10000);
-var F2 = (E2 - 10000*Math.floor(F1));
-var G1 = (F2 / 1000);
-var G2 = (F2 - 1000*Math.floor(G1));
-var H1 = (G2 / 100);
-var H2 = (G2 - 100*Math.floor(H1));
-var I1 = (H2 / 10);
-var I2 = (H2 - 10*Math.floor(I1));
-var J1 = (I2 / 1);
-var J2 = (I2 - 1*Math.floor(J1));
+var B1 = (A1 / 10000000);
+var B2 = (A1 - 10000000*Math.floor(B1));
+var C1 = (B2 / 1000000);
+var C2 = (B2 - 1000000*Math.floor(C1));
+var D1 = (C2 / 100000);
+var D2 = (C2 - 100000*Math.floor(D1));
+var E1 = (D2 / 10000);
+var E2 = (D2 - 10000*Math.floor(E1));
+var F1 = (E2 / 1000);
+var F2 = (E2 - 1000*Math.floor(F1));
+var G1 = (F2 / 100);
+var G2 = (F2 - 100*Math.floor(G1));
+var H1 = (G2 / 10);
+var H2 = (G2 - 10*Math.floor(H1));
+var I1 = (H2 / 1);
+var I2 = (H2 - 1*Math.floor(I1));
+
 
 var Multiplos = (Constante[2]*Math.floor(B1))+(Constante[3]*Math.floor(C1))+
                 (Constante[0]*Math.floor(D1))+(Constante[1]*Math.floor(E1))+
